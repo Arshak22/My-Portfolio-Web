8b5c0d49ec7327804b2e474e2cdded14f144bd45
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-06-02 21:50:41 +0400

    Added Contact Email Functionality

--- my-portfolio-web/src/Components/ContactMe/index.jsx
+++ my-portfolio-web/src/Components/ContactMe/index.jsx
@@ -1,23 +1,75 @@
-import React from "react";
+import React, { useRef, useState } from 'react';
 import './style.css';
+import emailjs from 'emailjs-com';
 
 export default function ContactMe() {
-    return(
-    <div className="contactMe">
-        <div className="form">
-            <h1>Reach Out to Me</h1>
-            <p>Ride the waves of Poseidon's realm. Reach out using the form below, and let's embark on a digital odyssey together.</p>
-            <div className="inputs">
-                <div>
-                    <input type="text" placeholder="Full Name" className="formInp"/>
-                    <input type="email" name="email" id="email" placeholder="Email Address" className="formInp"/>
-                </div>
-                <textarea name="message" id="message" cols="30" rows="5" placeholder="Message" className="formInp"></textarea>
+  const form = useRef();
+  const [message, setMessage] = useState('');
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_8tfn3j5',
+        'template_9dqwxue',
+        form.current,
+        'FBjZsrEn67HWqOmf1'
+      )
+      .then(
+        (result) => {
+          setMessage('Email was succesfully sent');
+        },
+        (error) => {
+          setMessage(
+            'Oops something went wrong, please check your input fields'
+          );
+        }
+      );
+
+    form.current.reset();
+  };
+
+  return (
+    <div className='contactMe'>
+      <div className='form'>
+        <h1>Reach Out to Me</h1>
+        <p>
+          Ride the waves of Poseidon's realm. Reach out using the form below,
+          and let's embark on a digital odyssey together.
+        </p>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className='inputs'>
+            <div>
+              <input
+                type='text'
+                name='from_name'
+                placeholder='Full Name'
+                className='formInp'
+                required
+              />
+              <input
+                type='email'
+                name='from_email'
+                placeholder='Email Address'
+                className='formInp'
+                required
+              />
             </div>
-            <button className="wave-btn formSubmit">
-                <span className="wave-btn_text">Send</span>
-                <span className="wave-btn_waves"></span>
-            </button>
-        </div>
-    </div>);
-};
+            <textarea
+              name='message'
+              placeholder='Message'
+              className='formInp'
+              required
+            ></textarea>
+          </div>
+          <button type='submit' className='wave-btn formSubmit'>
+            <span className='wave-btn_text'>Send</span>
+            <span className='wave-btn_waves'></span>
+          </button>
+          <p>{message}</p>
+        </form>
+      </div>
+    </div>
+  );
+}
